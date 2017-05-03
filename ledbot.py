ce2cb17b194100845464564b57550e560f0b3f7e
JGabrielFreitas <jgabrielafreitas@gmail.com>
2017-05-02 23:36:55 -0300

    led script bot

---
+++ ledbot.py
@@ -0,0 +1,38 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
+
+BOT_TOKEN   = "351567687:AAHgnFGUOG0Cg7sEIEM0PoEbywOLNmzkESg"
+updater     = Updater(BOT_TOKEN)
+pin_default = 12        
+ON 			= 1
+OFF 		= 0                  
+GPIO.setmode(GPIO.BOARD)
+
+#Define pin 12 as output
+GPIO.setup(pin_default, GPIO.OUT)
+
+# def start(bot, update):
+    
+
+# def stop(bot, update):
+
+def ledon(pin_led = pin_default):
+    GPIO.output(pin_led, ON)
+
+def ledoff(pin_led = pin_default):
+    GPIO.output(pin_led, OFF)
+    
+
+def message(bot, update):
+	print(update.message.text)
+
+updater.dispatcher.add_handler(CommandHandler('on', ledon))
+updater.dispatcher.add_handler(CommandHandler('off', ledoff))
+updater.dispatcher.add_handler(MessageHandler(Filters.text, message))
+
+updater.start_polling()
+updater.idle()
+
+
+
